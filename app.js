8d95ea43cb15350fc7cf7decd2175ee92669205e
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-17 23:09:21 +0100

    working on styles

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('hello');
+
+
+// https://www.google.com/search?q=how+to+move+button+to+input+field+css&oq=how+to+move+button+to+input+field+css&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTI2MjEzajBqNKgCALACAA&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:d1818379,vid:kr5nR6qHScA,st:0 
