c8e7c0fa490e1c9393783722101c216736324f80
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-21 12:05:08 +0100

    cleaning the code, JS is finished

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 "use strict";
 
-// const hidden = document.querySelector(".hidden");
-// const message = document.querySelector(".message");
-
-// console.log(submit);
-// console.log(hidden);
-// console.log(message);
-
-// submit.addEventListener("click", () => {
-//   document.querySelector(".message").classList.toggle("hidden");
-// });
-
 const submit = document.querySelector(".submit");
-const form = document.querySelector("form");
 const emailInput = document.querySelector('input[type="email"]');
 const errorMessage = document.querySelector(".message");
-const error = document.querySelector(".error");
+const errorIcon = document.querySelector(".error-icon");
 
 submit.addEventListener("click", (event) => {
-  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
   const emailValue = emailInput.value.trim();
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
 
 
   if (!isValidEmail) {
     errorMessage.classList.remove("hidden");
     event.preventDefault();
-    error.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
   } else {
     errorMessage.classList.add("hidden");
   }
